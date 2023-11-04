f6e6cdb8a35c7921f9272273d10093360bf7a9f9
Daniel Böhmer <post@daniel-boehmer.de>
2023-11-04 08:45:01 +0100

    Move quirks from Script::Deploy to Coocook::DeploymentHandler

--- lib/Coocook/DeploymentHandler.pm
+++ lib/Coocook/DeploymentHandler.pm
@@ -5,4 +5,35 @@ use warnings;
 
 use parent 'DBIx::Class::DeploymentHandler';
 
+use File::Spec;
+
+sub new {
+    my ( $self, $args ) = @_;
+
+    my $script_directory = $args->{script_directory};
+
+    $script_directory eq 'share/ddl'
+      or warn "Unexpected script_directory: $script_directory";
+
+    # Since Perl 5.26 @INC doesn't contain '.' anymore and
+    # includes of .pl files do require qualified paths.
+    # I tried './share/ddl' but DBIx::Class::DeploymentHandler finds files
+    # and removes the './' part. Absolute path works.
+    local $args->{script_directory} = File::Spec->rel2abs($script_directory);
+
+    $self->next::method($args);
+}
+
+{    # workaround to let Producer::SQLite NOT mangle index names
+    use SQL::Translator::Producer::SQLite;
+    no warnings 'redefine';
+    my $orig = \&SQL::Translator::Producer::SQLite::mk_name;
+    *SQL::Translator::Producer::SQLite::mk_name = sub {
+        my ($name) = @_;
+        my $ret = $orig->(@_);
+        $ret eq $name or $ret eq qq("$name") or warn "mk_name(@_) => $ret";
+        return $name;
+    };
+}
+
 1;
--- lib/Coocook/Script/Deploy.pm
+++ lib/Coocook/Script/Deploy.pm
@@ -1,35 +1,16 @@
 package Coocook::Script::Deploy;
 
-# ABSTRACT: script for database maintance based on App::DH
+# ABSTRACT: script for database maintenance based on App::DH
 
 use Moose;
 use namespace::autoclean;
 
 use Coocook::DeploymentHandler;
-use File::Spec;
 use PerlX::Maybe;
-
-{    # workaround to let Producer::SQLite NOT mangle index names
-    use SQL::Translator::Producer::SQLite;
-    no warnings 'redefine';
-    my $orig = \&SQL::Translator::Producer::SQLite::mk_name;
-    *SQL::Translator::Producer::SQLite::mk_name = sub {
-        my ($name) = @_;
-        my $ret = $orig->(@_);
-        $ret eq $name or $ret eq qq("$name") or warn "mk_name(@_) => $ret";
-        return $name;
-    };
-}
 
 extends 'App::DH';
 
 has '+schema' => ( default => 'Coocook::Schema' );
-
-# Since Perl 5.26 @INC doesn't contain '.' anymore and
-# includes of .pl files do require qualified paths.
-# I tried './share/ddl' but DBIx::Class::DeploymentHandler finds files
-# and removes the './' part. Absolute path works.
-has '+script_dir' => ( default => File::Spec->rel2abs('share/ddl') );
 
 sub _build_database { [qw< SQLite PostgreSQL >] }
 
