b157b3521b527eb3dc5dfcc727d577a2d3e30b1c
Daniel Böhmer <post@daniel-boehmer.de>
2023-11-14 02:49:18 +0100

    Revisit suggested units
    - rename from "preferred units" to "suggested units"
    
    - remove old logic of locking units in use which can't be deselected
    
    - display usage of units by font weight and in numbers

--- lib/Coocook/Controller/Article.pm
+++ lib/Coocook/Controller/Article.pm
@@ -30,7 +30,7 @@ sub index : GET HEAD Chained('/project/base') PathPart('articles') Args(0)
         my $edit_action   = $self->action_for('edit');
         my $delete_action = $self->action_for('delete');
 
-        my %shop_sections = map { $_->id => $_ } $c->stash->{shop_sections}->@*;
+        my %shop_sections = map { $_->{id} => $_ } $c->stash->{shop_sections}->@*;
 
         my $articles = $c->project->articles->sorted->hri;
 
@@ -88,17 +88,12 @@ sub edit : GET HEAD Chained('base') PathPart('') Args(0) RequiresCapability('vie
 
     my $article = $c->stash->{article};
 
-    $c->forward('fetch_project_data');
+    $c->forward( 'fetch_project_data', [$article] );
     $c->forward('dishes_recipes');
 
-    my $units        = $article->units;
-    my $units_in_use = $article->units_in_use;
+    my $units = $article->units;
 
-    $c->stash(
-        submit_url     => $c->project_uri( $self->action_for('update'), $article->id ),
-        selected_units => { map { $_ => 1 } $units->get_column('id')->all },
-        units_in_use   => { map { $_ => 1 } $units_in_use->get_column('id')->all },
-    );
+    $c->stash( submit_url => $c->project_uri( $self->action_for('update'), $article->id ) );
 }
 
 =head1 CRUD ENDPOINTS
@@ -142,16 +137,30 @@ sub delete : POST Chained('base') Args(0) RequiresCapability('edit_project') {
 =cut
 
 sub fetch_project_data : Private {
-    my ( $self, $c ) = @_;
+    my ( $self, $c, $article ) = @_;
+
+    my $units = $c->project->units;
+
+    if ($article) {
+        $units = $units->with_number_of_ingredients_items( { article_id => $article->id } );
+
+    }
 
     $c->stash(
         default_shelf_life_days   => 7,
         default_preorder_servings => 10,
         default_preorder_workdays => 3,
-        shop_sections             => [ $c->project->shop_sections->sorted->all ],
-        units                     => [ $c->project->units->sorted->all ],
-
+        shop_sections             => [ $c->project->shop_sections->sorted->hri->all ],
+        units                     => [ $units->sorted->hri->all ],
     );
+
+    if ($article) {
+        my %selected_units = map { $_ => undef } $article->units->get_column('id')->all;
+
+        for my $unit ( $c->stash->{units}->@* ) {
+            $unit->{selected} = exists $selected_units{ $unit->{id} };
+        }
+    }
 }
 
 =head2 dishes_recipes()
@@ -233,8 +242,6 @@ sub update_or_insert : Private {
 
     my $articles_units = $article->articles_units;
 
-    my @units_in_use = $article->units_in_use->get_column('id')->all;
-
     my %selected_units = map { $_ => 1 } my @selected_units = $c->req->params->get_all('units');
     my %all_units      = map { $_ => 1 } my @all_units      = $c->project->units->get_column('id')->all;
     my %current_units = map { $_ => 1 } my @current_units = $articles_units->get_column('unit_id')->all;
@@ -242,11 +249,6 @@ sub update_or_insert : Private {
     for my $sent_id (@selected_units) {
         $all_units{$sent_id}
           or $c->detach( '/error/bad_request', ["Your browser sent an invalid unit ID."] );
-    }
-
-    for my $id (@units_in_use) { # this isn't input verification, the HTML form doesn't allow to do this
-        $selected_units{$id}
-          or $c->detach( '/error/bad_request', ["You’ve deselected a unit that is in use."] );
     }
 
     my @units_to_remove = grep { not $selected_units{$_} } @current_units;
--- t/controller_Article.t
+++ t/controller_Article.t
@@ -3,7 +3,7 @@ use Test2::V0;
 use lib 't/lib';
 use Test::Coocook;
 
-plan(14);
+plan(13);
 
 my $t = Test::Coocook->new();
 
@@ -36,17 +36,6 @@ subtest "invalid unit IDs" => sub {
 };
 
 $t->back();
-
-subtest "deselect units that are in use" => sub {
-    my $form_data = $update_req->content;
-    $form_data =~ s/&units=$_// or die for 2, 9999;    # remove IDs sent before
-
-    ok $t->post( $update_req->uri, content => $form_data );
-    $t->status_is(400);
-    $t->text_contains("in use");
-
-    $t->back();
-};
 
 # select unit 3 (liters), I couldn't get this working by passing units=>[...] to submit_form()
 $t->form_number(3);
