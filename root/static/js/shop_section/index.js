92ea3fc05ee47a3cfc0038e2b6fa1598f3c430d3
Kurt Roscher <ku.roscher@abenteuerkompass.eu>
2023-12-30 00:05:12 +0100

    Improve shop section index page

--- root/static/js/shop_section/index.js
+++ root/static/js/shop_section/index.js
@@ -2,12 +2,22 @@ function setModal(e) {
     let url = e.currentTarget.dataset.url;
     let name = e.currentTarget.dataset.name;
 
-    document.getElementById("update-section").action = url;
+    document.getElementById("update-section-form").action = url;
     document.getElementById("section-name").innerText = name;
     document.getElementById("ss-new-name").value = name;
-    document.getElementById("updateSection").addEventListener("shown.bs.modal", () => document.getElementById("ss-new-name").focus());
+    document
+        .getElementById("update-section")
+        .addEventListener("shown.bs.modal", () =>
+            document.getElementById("ss-new-name").focus()
+        );
 }
 
-document.querySelectorAll(".update").forEach(elem => elem.addEventListener("click", setModal));
+document
+    .querySelectorAll(".update")
+    .forEach((elem) => elem.addEventListener("click", setModal));
 
-document.getElementById("addSection").addEventListener("shown.bs.modal", () => document.getElementById("ss-name").focus());
+document
+    .getElementById("add-section")
+    .addEventListener("shown.bs.modal", () =>
+        document.getElementById("ss-name").focus()
+    );
