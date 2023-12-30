f0c8460cee9d21d278bd811b882cf27edbee8e6c
Daniel Böhmer <post@daniel-boehmer.de>
2023-12-31 00:49:36 +0100

    Optimize older call of results_exist_as_query()

--- lib/Coocook/Schema/Component/ResultSet/ArticleOrUnit.pm
+++ lib/Coocook/Schema/Component/ResultSet/ArticleOrUnit.pm
@@ -10,7 +10,7 @@ my @relationships = qw<
   items
 >;
 
-=head2 in_use()
+=head2 in_use($cond?)
 
 Returns a new resultset with articles/units which have any of
 dish ingredients, recipe ingredients or purchase list items.
@@ -18,12 +18,10 @@ dish ingredients, recipe ingredients or purchase list items.
 =cut
 
 # TODO could this be self-relation 'in_use' in Article/Unit + 'units_in_use' in Article?
-sub in_use {
-    my $self = shift;
-
+sub in_use ( $self, $cond = undef ) {
     return $self->search(
         [    # OR
-            map { $self->correlate($_)->search(@_)->results_exist_as_query } @relationships
+            map { $self->correlate($_)->results_exist_as_query($cond) } @relationships
         ]
     );
 }
