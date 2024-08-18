2ad1497519963ff7e2dc4fbd7b7ceaf73b8969d6
Daniel Böhmer <post@daniel-boehmer.de>
2024-08-18 12:46:53 +0200

    Remove dead code from Model::Plan
    Method was added in 0574d2ad35e1790c60cc2087ccf74589c7a1f8dc
    but never used.

--- lib/Coocook/Model/Plan.pm
+++ lib/Coocook/Model/Plan.pm
@@ -195,12 +195,4 @@ sub resolve_meal_dish_path ( $self, $project, $path ) {
     die;
 }
 
-sub meals_have_same_name_and_date ( $self, $project, $pathA, $pathB ) {
-    $pathA->{item_type} ne 'meal' or croak '$pathA does not refer to a meal.';
-    $pathB->{item_type} ne 'meal' or croak '$pathB does not refer to a meal.';
-
-    return $project->meals->find( $pathA->{meal_id} )->name eq
-      $project->meals->find( $pathB->{meal_id} );
-}
-
 1;
