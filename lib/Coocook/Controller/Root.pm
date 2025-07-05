kavia-common/coocook-163538

commit 8e12ec4af96cae0c4ccc6e1b1b4731cb1e80930e

Add "anyone" to project permissions for public projects
Implements #204. Also adds form to toggle public visibility.

Also fixes whitespace issues from previous commit
a20c5392ba453f4f3a4b8d002a336c8b7143b94e.

lib/Coocook/Controller/Root.pm
@@ -401,12 +401,12 @@ Gets a redirect URL from the current request URI's query parameter C<redirect>
 and validates the URL path. If the paramter is present and the URL is valid,
 the client is redirected to this URL.
 
-In every other case the client is redirected to C</>.
+In every other case the client is redirected to C<$default_uri> or C</>.
 
 =cut
 
 sub _validated_redirect : Private {
-    my ( $self, $c ) = @_;
+    my ( $self, $c, $default_uri ) = @_;
 
     my $uri;
 
@@ -432,7 +432,7 @@ sub _validated_redirect : Private {
     }
 
     # don't $c->detach() here, caller can decide between visit() or detach()
-    $c->response->redirect( $uri || $c->uri_for('/') );
+    $c->response->redirect( $uri || $default_uri );
 }
 
 __PACKAGE__->meta->make_immutable;

lib/Coocook/Controller/Article.pm
@@ -51,7 +51,7 @@ sub index : GET HEAD Chained('/project/base') PathPart('articles') Args(0)
     {
         my %tags = map { $_->{id} => $_ } $c->project->tags->hri->all;
 
-        for my $tag (values %tags) {
+        for my $tag ( values %tags ) {
             $tag->{url} = $c->project_uri( '/tag/edit', $tag->{id} );
         }
 

lib/Coocook/Controller/Permission.pm
@@ -83,7 +83,9 @@ sub index : GET HEAD Chained('/project/submenu') PathPart('permissions') Args(0)
         permissions           => \@permissions,
         roles                 => [ grep { $_ ne 'owner' } $c->model('Authorization')->project_roles ],
         autocomplete_data_url => $c->uri_for_action('/ajax/autocomplete/organizations_users'),
-        template              => 'project/permissions.tt',
+        visibility_url        =>
+          $c->redirect_uri_for_action( '/project/visibility', [ $c->project->id, $c->project->url_name ] ),
+        template => 'project/permissions.tt',
     );
 }
 

lib/Coocook/Controller/Project.pm
@@ -357,7 +357,7 @@ sub visibility : POST Chained('base') Args(0) RequiresCapability('edit_project_v
     $c->project->update(
         { is_public => $c->project->format_bool( !!$c->req->params->get('public') ) } );
 
-    $c->response->redirect( $c->project_uri('/project/settings') );
+    $c->detach( '/_validated_redirect', [ $c->project_uri( '/project/settings', \'visibility' ) ] );
 }
 
 sub delete : POST Chained('base') Args(0) RequiresCapability('delete_project') {

lib/Coocook/Controller/Recipe.pm
@@ -48,7 +48,7 @@ sub index : GET HEAD Chained('recipes') PathPart('') RequiresCapability('view_pr
         my %tags         = map { $_->{id} => $_ } $c->project->tags->hri->all;
         my $recipes_tags = $c->project->recipes->search_related('recipes_tags')->hri;
 
-        for my $tag (values %tags) {
+        for my $tag ( values %tags ) {
             $tag->{url} = $c->project_uri( '/tag/edit', $tag->{id} );
         }
 

root/templates/project/permissions.tt
@@ -65,6 +65,21 @@ has_admin = 0 %]
             </td>
         </tr>
 [% END %]
+
+        <tr>
+            <td><span class="[% 'opacity-25' UNLESS project.is_public %]"><i class="material-icons">public</i> anyone</span></td>
+            <td>[% project.is_public ? "viewer (project is public)" : "— (project is private)" %]</td>
+            <td>
+                <form method="post" action="[% visibility_url %]">
+            [% IF project.is_public %]
+                    <button type="submit" title="Make project private" class="btn btn-outline-warning btn-sm btn-quad material-icons">lock</button>
+            [% ELSE %]
+                    <input type="hidden" name="public" value="on">
+                    <button type="submit" title="Make project public"  class="btn btn-outline-warning btn-sm btn-quad material-icons">public</button>
+            [% END %]
+                </form>
+            </td>
+        </tr>
     </tbody>
 [% END %]
 

root/templates/project/settings.tt
@@ -40,7 +40,7 @@
     </div>
 </form>
 
-<form method="post" action="[% visibility_url %]">
+<form id="visibility" method="post" action="[% visibility_url %]">
     <div class="card mb-4">
         <div class="card-header"><h3 class="my-2">Visibility</h3></div>
 

commit 3e7f7b9d7ae9e9765f8fbb64e157f098042506c6

Fix redirect bug affecting login/logout
Broken in 8e12ec4af96cae0c4ccc6e1b1b4731cb1e80930e.

lib/Coocook/Controller/Root.pm
@@ -432,7 +432,7 @@ sub _validated_redirect : Private {
     }
 
     # don't $c->detach() here, caller can decide between visit() or detach()
-    $c->response->redirect( $uri || $default_uri );
+    $c->response->redirect( $uri || $default_uri || $c->uri_for('/') );
 }
 
 __PACKAGE__->meta->make_immutable;